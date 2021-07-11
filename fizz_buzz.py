e5c31ac4d89e10bc2a96caa36f283291ff9b618c
savitadevi <83698279+savitadevi@users.noreply.github.com>
2021-07-11 22:50:41 +0530

    Add files via upload

---
+++ fizz_buzz.py
@@ -0,0 +1,18 @@
+# def number(num):
+#     i=1
+#     while i<=num:
+#         if i%3==0 and i%5==0:
+#             print(i,"fizzbuzz")
+#         elif i%3==0:
+#             print (i,"fizz")
+#         elif i%5==0:
+#             print(i,"buzz")
+#         else:
+#             print(i)
+#         i=i+1
+# number(int(input("enter the num"))) 
+
+
+
+
+
